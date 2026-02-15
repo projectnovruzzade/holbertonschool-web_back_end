375102a4d6cc5b3b1923d172a329095d856c6772
root <tunarwork99@gmail.com>
2026-02-15 19:38:42 +0000

    task_6

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+  this is external
+"""
+from typing import List
+
+def sum_list(input_list: List[float]) -> str:
+    """
+    this is internal
+    """
+    return sum(input_list)
