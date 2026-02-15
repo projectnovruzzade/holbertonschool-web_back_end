e07b8ba63a636eb33192e01374d6ba1adcf098bb
root <tunarwork99@gmail.com>
2026-02-15 19:44:05 +0000

    task_7

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,10 +2,10 @@
 """
   this is external
 """
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int  | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     this is internal
     """
