844a97ccb196761912dad49184fa3948060de3f1
root <tunarwork99@gmail.com>
2026-02-15 19:51:48 +0000

    end

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+  this is external
+"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
