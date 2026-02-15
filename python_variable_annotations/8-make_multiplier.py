f7c7f096bc79bed8e08566e19d84d5fc94e50d4c
root <tunarwork99@gmail.com>
2026-02-15 19:50:18 +0000

    task_9

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -7,6 +7,12 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    ok
+    """
     def multiply(value: float) -> float:
+        """
+         ok
+        """
         return value * multiplier
     return multiply
