87d57ab4c3f003155611c2159fb428068e2298a5
root <tunarwork99@gmail.com>
2026-02-15 19:42:17 +0000

    task_7

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+  this is external
+"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int  | float]) -> float:
+    """
+    this is internal
+    """
+    return sum(mxd_lst)
