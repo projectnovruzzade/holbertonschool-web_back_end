d2b8431436492db5bc6cc08cbc6009dadd54c4a0
root <tunarwork99@gmail.com>
2026-02-15 19:48:45 +0000

    task_9

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+  this is external
+"""
+
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    def multiply(value: float) -> float:
+        return value * multiplier
+    return multiply
