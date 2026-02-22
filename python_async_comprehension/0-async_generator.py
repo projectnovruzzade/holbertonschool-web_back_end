511dedaf7c10e6d7c37d678feddb680f15ae5e5d
root <tunarwork99@gmail.com>
2026-02-22 19:13:55 +0000

    t-1

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""ok"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    ok
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
