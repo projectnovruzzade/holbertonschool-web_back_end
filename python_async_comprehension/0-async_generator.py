ea070d2a215cbf2229f68a252d2903f49d35bb4f
root <tunarwork99@gmail.com>
2026-02-22 19:16:52 +0000

    1

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""ok"""
+"""ok ."""
 
 import asyncio
 import random
+from typing import Generator
 
-
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     ok
     """
