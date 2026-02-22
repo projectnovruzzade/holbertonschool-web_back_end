6059192256c8fe4c6904bbf16b521d73d63c2dc8
root <tunarwork99@gmail.com>
2026-02-22 18:51:35 +0000

    task_2

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,12 +4,13 @@ okay
 """
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ ok"""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    delays = []
+    delays: List[float] = []
     for coro in asyncio.as_completed(tasks):
         result = await coro
         delays.append(result)
