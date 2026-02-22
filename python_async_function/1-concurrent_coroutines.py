f0b53a5ba4733be2c2bc73f1ea81a1a61a4e9994
root <tunarwork99@gmail.com>
2026-02-22 19:00:57 +0000

    2

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ 
+    """
     okay everythis is
     """
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
