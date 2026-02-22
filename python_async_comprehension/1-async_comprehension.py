975800d9e37eb8ddde94774b8e68122387c8944c
root <tunarwork99@gmail.com>
2026-02-22 19:51:24 +0000

    2

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,9 +3,8 @@
 oka
 """
 
-
-async_generator = __import__('0-async_generator').async_generator
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
