06c207f74a38f74bb8b7d0300ccf8b9688035548
root <tunarwork99@gmail.com>
2026-02-22 13:30:20 +0000

    task_1

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+okay
+"""
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    rndm = random.random() * max_delay
+    await asyncio.sleep(rndm)
+    return rndm
