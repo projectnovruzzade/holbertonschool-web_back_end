fccd78a332e4a864d339b532918051a65520bbea
root <tunarwork99@gmail.com>
2026-02-22 13:34:17 +0000

    task_1

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -8,6 +8,9 @@ import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """
+    ok
+    """
     rndm = random.random() * max_delay
     await asyncio.sleep(rndm)
     return rndm
