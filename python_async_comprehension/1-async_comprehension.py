4263c116bd827504fbc0aa5a7e3c29fe8dd6f6dc
root <tunarwork99@gmail.com>
2026-02-22 19:48:24 +0000

    2

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+oka
+"""
+
+from 0-async_generator import async_generator
+
+
+async def async_comprehension() -> list[float]:
+    """
+    oka
+    """
+    return [i async for i in async_generator()]
