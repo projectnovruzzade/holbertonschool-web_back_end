9d9f7b2463480adf11238cef2cbbb48c298e0f00
root <tunarwork99@gmail.com>
2026-04-13 18:10:20 +0000

    task_4

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,7 @@
 -- Hello kitty
 SELECT 
     band_name,
-    (CASE 
-        WHEN split IS NULL THEN 2024 - formed
-        ELSE split - formed
-    END) AS lifespan
+    (2024 - formed) AS lifespan
 FROM bands
-WHERE genre LIKE '%Glam rock%'
+WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
